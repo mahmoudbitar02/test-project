adb2ca288bfbc9226a0afff52480d8e7684a46f3
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2022-12-30 18:20:08 +0100

    add media

--- test12/models.py
+++ test12/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=75)
+    describtion = models.TextField(max_length=5000)
+    image = models.ImageField(upload_to='test12')
